de5473b3b40d523e698a05ef6c74fbdaac77e791
chrisumurerwa <chrisumurerwa1@gmail.com>
2025-04-02 13:13:39 +0200

    changes

--- Routes/IndexRouting.js
+++ Routes/IndexRouting.js
@@ -1,12 +1,12 @@
 import express from "express";
 import book from "./BookAppRoute.js"
-import Contact from "./ContactRoute.js"
+
 import User from "./userRoute.js"
 import { getAppointments } from "../controller/BookAppController.js";
 import  {createMentorApplication, getAllMentorApplications} from "../controller/MentorAppController.js"
 import { submitGetInTouchForm, getAllMessages } from "../controller/GetInTouchController.js";
 const route = express.Router();
-route.use("/contact", Contact);
+
 route.use("/user", User);
 route.use("/bookAppointment", book);
 route.use("/getAppointments/:id", getAppointments);
